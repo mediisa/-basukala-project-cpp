17c54f156394497e3dc402d9c0391c33b56709fb
mediisa <mediisa598@gmail>
2026-02-11 17:21:58 +0330

    add new file for writing and read from file

---
+++ WRFile.h
@@ -0,0 +1,22 @@
+#ifndef WRFILE_H
+#define WRFILE_H
+
+#include "common.h"
+
+
+void loadUsersFromFile();
+void saveUsersToFile();
+
+
+void loadProductsFromFile();
+void saveProductsToFile();
+
+
+void loadOrdersFromFile();
+void saveOrdersToFile();
+
+
+void loadPackagesFromFile();
+void savePackagesToFile();
+
+#endif
