a2da4d8d6ead2a745861f7e8b3c2fe1dcb075314
mediisa <mediisa598@gmail>
2026-02-20 14:25:25 +0330

    Change the menu function

--- admin.h
+++ admin.h
@@ -1,19 +1,39 @@
 #ifndef ADMIN_H
 #define ADMIN_H
 
-#include "common.h"
+#include "Models.h"
 
+class UserService;
 
-void displayProducts();
-void displayUsers();
-void displayPendingPackages();
+class AdminService {
+public:
+    explicit AdminService(UserService& userService);
 
-void addProduct();
-void deleteProduct();
+    void adminMenu(User& adminUser);
 
-void processNextPackage();
+private:
+    UserService& userService_;
+    std::string promptLine(const std::string& prompt);
+    bool tryParseInt(const std::string& s, int& out);
 
+    void displayProducts();
+    void printCategoryOptions();
+    void displayProductsByCategorySorted(const std::string& categoryFilter);
+    void selectProductByCategoryByNumber();
+    void displayTopSellingProduct();
+    void displayByName();
+    void displayByPriceProduct();
 
-void adminMenu(User &adminUser);
+    void displayUsers();
+
+    void addProduct();
+    void deleteProduct();
+    void editProduct();
+
+    void processNextPackage();
+    void displayPendingPackages();
+    
+};
+//extern AdminService adminService;
 
 #endif
