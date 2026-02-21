efbc8cb1e256de638a017a491da6eed4a408c100
mediisa <mediisa598@gmail>
2026-02-21 17:50:38 +0330

    newMenu for customer

--- customer.h
+++ customer.h
@@ -1,40 +1,49 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
+
 #include "Models.h"
 
 class UserService;
 
 class CustomerService {
-public:
+    public:
     struct RemovedItem {
         OrderItem item;
-        size_t originalIndex;
+        size_t rowIndex;
     };
 
     explicit CustomerService(UserService& userService);
 
     void customerMenu(User& user);
 
-private:
-    UserService& userService_;
+    private:
+        UserService& userService_;
 
     std::string promptLine(const std::string& prompt);
     bool askYesNo(const std::string& prompt);
     bool tryParseInt(const std::string& s, int& out);
     int totalQuantityInCartFor(const std::vector<OrderItem>& cart, const std::string& productName);
-
+    int totalItem(const std::vector<OrderItem>& cart);
     void printCategoryOptions();
-    std::vector<Product*> displayProductsByCategorySorted(const std::string& categoryFilter);
+    
+    
+    void displayProducts();
+    void selectProductByCategoryByNumber();
+    void displayByTopSellingNamePriceProduct(int choice);
 
-    void searchProductsWithSuggestions();
-    Product* selectProductByName();
-    Product* selectProductFromSuggestions();
-    Product* selectProductByCategoryByNumber();
+    
+    void store(std::vector<OrderItem>& cart , User& user);
+    void addToCart(std::vector<OrderItem>& cart);    
+    void searchProductsWithSuggestions(std::vector<OrderItem>& cart);
 
-    void addToCart(std::vector<OrderItem>& cart);
     void addSelectedProductToCart(std::vector<OrderItem>& cart, Product* product);
+    std::vector<Product*> displayProductsByCategorySorted(const std::string& categoryFilter);
+    Product* selectProductByName();
+    Product* selectProductFromSuggestions(std::vector<std::string>& suggestions);
+    Product* selectProductByCategoryByNumber1();
 
+   // void ModifyCart(std::vector<OrderItem>& cart );
     void removeFromCart(std::vector<OrderItem>& cart, std::vector<RemovedItem>& removedStack);
     void undoLastRemoval(std::vector<OrderItem>& cart, std::vector<RemovedItem>& removedStack);
     void restoreRemovedByNumber(std::vector<OrderItem>& cart, std::vector<RemovedItem>& removedStack);
@@ -43,9 +52,11 @@ private:
     void checkout(User& user, std::vector<OrderItem>& cart);
     void addFunds(User& user);
 
+    void displayOrderHistoryFromOrders(const User& user);
     char promptDestinationCity();
+
+    void displayTopSellingProduct(int topN);
+    void displayUserInfo(User& user);
+
 };
-
-//extern CustomerService customerService;
-
 #endif
