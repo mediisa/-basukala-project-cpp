91b7b36b3b2803309dd2de32b7a16742f39218e5
mediisa <mediisa598@gmail>
2026-02-25 10:45:01 +0330

    Models.h i made member pravit , write func set and get

--- Models.h
+++ Models.h
@@ -4,26 +4,26 @@
 #include <string>
 #include <vector>
 
-class OrderItem {
-public:
+struct OrderItem {
     std::string productName;
     int quantity;
     double unitPrice;
 
-    OrderItem();
-    OrderItem(const std::string& name, int qty, double price);
+OrderItem()
+    : productName(""), quantity(0), unitPrice(0.0) {}
 
-    double lineTotal() const;
+OrderItem(const std::string& name, int qty, double price)
+    : productName(name), quantity(qty), unitPrice(price) {}   
 };
 
 class Product {
-public:
+private:
     std::string name;
     std::string category;
     double price;
     int stock;
     int soldCount;
-
+public:
     Product();
     Product(const std::string& name, const std::string& category, double price, int stock);
 
@@ -35,10 +35,17 @@ public:
     void setPrice(double p);
     void setName(const std::string& n);
     void setCategory(const std::string& c);
+
+    std::string getname() const;
+    std::string getcategory() const;
+    double getprice() const;
+    int getstock() const;
+    int getsoldCount() const;
+
 };
 
 class Order {
-public:
+private:
     std::string id;
     std::string username;
     std::vector<OrderItem> items;
@@ -46,16 +53,27 @@ public:
     char destinationCity;
     std::string address;
     std::string status;
+public:
+    Order(const std::string& id , const std::string& username , const std::vector<OrderItem>& items , 
+           const double& totalCost , const char& destinationCity , const std::string& address , const std::string& status);
 
-    Order();
+    Order(const std::string& id , const std::string& username , const std::vector<OrderItem>& items , 
+            const char& destinationCity , const std::string& address , const std::string& status);
 
-    int totalItems() const;
-    double subtotal() const;
-    void recomputeTotal(double shippingCost);
+std::string getid() const;
+std::string getusername() const;
+std::vector<OrderItem> getitems() const;
+double gettotalCost() const;
+char getdestinationCity() const;
+std::string getaddress() const;
+std::string getstatus() const;
+void settotalCost(double newtotalCost) ;
+void setStatus(std::string newstatus);
+
 };
 
 class Package {
-public:
+private:
     std::string id;
     std::string orderId;
     std::string username;
@@ -67,25 +85,61 @@ public:
     std::string routeDisplay;
     int routeDistance;
     long long enqueueIndex;
-
+public:
     Package();
-
+    Package(const std::string& id , const std::string& orderId , const std::string& username ,
+            const std::vector<OrderItem>& items , const char& destinationCity , const std::string& address ,
+            const std::string& status , const std::string& routeDisplay);
     int computeScoreFromItems() const;
     void updateRoute(const std::string& routeText, int distance);
+    
+    std::string getid() const;
+    std::string getorderId() const;
+    std::string getusername() const;
+    std::vector<OrderItem> getitems() const;
+    char getdestinationCity() const;
+    std::string getaddress() const;
+    int getscore() const;
+    std::string getstatus() const;
+    std::string getrouteDisplay() const;
+    int getrouteDistance() const;
+    long long getenqueueIndex() const;
+    void setstatus(std::string newstatus);
+    void setrouteDistance(int newrouteDistance);
+    void setenqueueIndex(long long newenqueueIndex);
+    void setrouteDisplay(std::string routeDisplay);
+    void setscore(int newscore);
+    
 };
 
 class User {
-public:
+private:
     std::string username;
     std::string password;
+    std::string passwordHash;   
+    std::string salt;
     std::string role;
-
     double balance;
     int score;
     std::vector<std::string> orderHistory;
-
+public:
     User();
-    User(const std::string& username, const std::string& password, const std::string& role);
+    User(const std::string& username, const std::string& password, const std::string& passwordHash, const std::string& role);
+    User(const std::string& username, const std::string& salt, const std::string& password, const std::string& passwordHash, const std::string& role);
+
+    std::string getusername() const;
+    std::string getpassword() const;
+    std::string getpasswordHash() const;
+    std::string getsalt() const;
+    std::string getrole() const;
+    double getbalance() const;
+    int getscore() const;
+    std::vector<std::string> getorderHistory() const;
+
+    void reduceBlance(int qty);
+    void addBlance(int qty);
+    void addScore(int qty);
+
 };
 
 #endif
